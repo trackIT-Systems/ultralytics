19e417bd680eff4967cb8a9ff16e2bbe28d89a32
Michael Fuchs <fuchs@trackit.systems>
2025-06-02 11:33:06 +0000

    add local argument to exporter to only save model locally

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -224,7 +224,10 @@ class Exporter:
             _callbacks (dict, optional): Dictionary of callback functions. Defaults to None.
         """
         self.args: SimpleNamespace = get_cfg(cfg, overrides)
-        self.output_path: str = f'/yolobat-train/var/model_export/yolobat/{self.args.version}/{self.args.version}_{self.args.release}'
+        if self.args.local:
+            self.output_path: str = f'/yolobat-train/var/model_export_local/yolobat/{self.args.version}/{self.args.version}_{self.args.release}'
+        else:
+            self.output_path: str = f'/yolobat-train/var/model_export/yolobat/{self.args.version}/{self.args.version}_{self.args.release}'
         os.makedirs(self.output_path, exist_ok=True)
 
         if self.args.format.lower() in {"coreml", "mlmodel"}:  # fix attempt for protobuf<3.20.x errors
